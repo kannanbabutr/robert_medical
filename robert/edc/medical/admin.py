6783303626426b88bb2674d35010c19a495ae95b
Kannan Babu <trkannanbabu@outlook.com>
2016-10-16 09:52:42 +0530

    17 tables added on 16 oct

--- robert/edc/medical/admin.py
+++ robert/edc/medical/admin.py
@@ -1,9 +1,20 @@
+#Study
 from .models import StudyMaster
 from .models import FormMaster
 from .models import CodeListMaster
 from .models import CodeListCodeName
 from .models import SdvStatusForAllStudy
 from .models import ReasonForChange
+from .models import CountryMaster
+from .models import MasterParameters
+from .models import LevelOne
+from .models import LevelTwo
+from .models import LevelThree
+from .models import StudyLock
+from .models import TimeZoneMaster
+from .models import DemoStudySignoff
+from .models import FormChange
+from .models import FunctionMaster
 
 from django.contrib import admin
 
@@ -13,6 +24,16 @@ admin.site.register(CodeListMaster)
 admin.site.register(CodeListCodeName)
 admin.site.register(SdvStatusForAllStudy)
 admin.site.register(ReasonForChange)
+admin.site.register(CountryMaster)
+admin.site.register(MasterParameters)
+admin.site.register(LevelOne)
+admin.site.register(LevelTwo)
+admin.site.register(LevelThree)
+admin.site.register(StudyLock)
+admin.site.register(TimeZoneMaster)
+admin.site.register(DemoStudySignoff)
+admin.site.register(FormChange)
+admin.site.register(FunctionMaster)
 
 
 # Register your models here.
