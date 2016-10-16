cbf516747cfdea7367075cb40bfbe6828b53154e
Kannan Babu <trkannanbabu@outlook.com>
2016-10-16 09:52:52 +0530

    17 tables added on 16 oct

--- robert/edc/medical/models.py
+++ robert/edc/medical/models.py
@@ -64,9 +64,83 @@ class ReasonForChange(models.Model):
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 
+class CountryMaster(models.Model):
+	country_id = models.IntegerField()
+	country_name = models.CharField(max_length=100)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+class MasterParameters(models.Model):
+	key_1 = models.CharField(max_length=500)
+	value_1 = models.CharField(max_length=500)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+class LevelOne(models.Model):
+	row_id = models.IntegerField()
+	level_one = models.CharField(max_length=100)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+class LevelTwo(models.Model):
+	row_id = models.IntegerField()
+	level_two = models.CharField(max_length=100)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+class LevelThree(models.Model):
+	row_id = models.IntegerField()
+	level_three = models.CharField(max_length=100)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
 
 
+class StudyLock(models.Model):
+	row_id = models.IntegerField()
+	study_type_id = models.CharField(max_length=200)
+	study_type = models.CharField(max_length=200)
+	study_lock = models.CharField(max_length=200)
+	study_comment = models.CharField(max_length=200)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
 
+
+class TimeZoneMaster(models.Model):
+	zone_id = models.IntegerField()
+	site_zone_id = models.CharField(max_length=500)
+	site_zone_info = models.CharField(max_length=500)	
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+
+class DemoStudySignoff(models.Model):
+	row_id = models.IntegerField()
+	site_id = models.TextField()
+	site_cluster_id = models.TextField()
+	patient_id = models.TextField()
+	data_validate = models.TextField()
+	user_name = models.TextField()
+	signoff_timezone = models.DateTimeField(default=timezone.now)
+	signoff_status= models.CharField(max_length=100)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+
+class FormChange(models.Model):
+	row_id = models.IntegerField()
+	form_id = models.CharField(max_length=200)
+	field_id = models.CharField(max_length=200)
+	field_label = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+class FunctionMaster(models.Model):
+	row_id = models.IntegerField()
+	study_id = models.IntegerField()
+	function_name = models.CharField(max_length=100)
+	function_code = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
 
 
 
