1aecb17d8a754a852e840474619578ce5599f049
Kannan Babu <trkannanbabu@outlook.com>
2016-10-16 08:57:49 +0530

    Added 16 oct

--- robert/edc/medical/admin.py
+++ robert/edc/medical/admin.py
@@ -1,8 +1,18 @@
 from .models import StudyMaster
 from .models import FormMaster
+from .models import CodeListMaster
+from .models import CodeListCodeName
+from .models import SdvStatusForAllStudy
+from .models import ReasonForChange
+
 from django.contrib import admin
 
 admin.site.register(StudyMaster)
 admin.site.register(FormMaster)
+admin.site.register(CodeListMaster)
+admin.site.register(CodeListCodeName)
+admin.site.register(SdvStatusForAllStudy)
+admin.site.register(ReasonForChange)
+
 
 # Register your models here.
